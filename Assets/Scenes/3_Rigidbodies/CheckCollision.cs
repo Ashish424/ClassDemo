1b6def706366d98e8cdacc5dc0f7f29d16cf4e1e
ASHISH AAPAN <ashishaapan@gmail.com>
2018-06-18 12:29:13 +0530

    init commit

---
+++ Assets/Scenes/3_Rigidbodies/CheckCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		Debug.Log("entered collision");
+	}
+	private void OnCollisionStay(Collision other) {
+		Debug.Log("collision stay");
+	}
+	void OnCollisionExit(Collision other)
+	{
+		Debug.Log("exit collision");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
