cce29db1459b1f125d3459940e08495ba8b947cf
ASHISH AAPAN <ashishaapan@gmail.com>
2018-06-21 12:25:22 +0530

    added continuous collision detection test

---
+++ Assets/Scenes/4_StructVsClasses/SvsC.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SvsC : MonoBehaviour {
+
+
+
+	
+	
+	class TestClass{
+
+
+		public float x;
+		public float y;
+
+	} 
+    struct TestStruct{
+
+	    public float x;
+	    public float y;
+
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	
+		TestClass tc = new TestClass();
+		TestClass tc2 = tc;
+		tc2.x =4;
+		
+		Debug.Log("value of tc.x "+tc.x+ " value of tc2.x "+tc2.x);
+		
+		
+		TestStruct tS = new TestStruct();
+		TestStruct tS2 = tS;
+		tS2.x = 2;
+		Vector3 z = new Vector3(0, 0, 1);
+		Quaternion q = new Quaternion(0,0,0,1);
+		
+
+		Debug.Log("value of ts.x "+tS.x+ " value of ts2.x "+tc2.x);
+
+
+
+
+	}
+	
+}
---
+++ Assets/Scenes/5_Continuous/Mover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour{
+
+
+
+	[Range(0,4000)][SerializeField] private float speed;
+
+
+	private void Awake(){
+
+		myRigidbody2D = GetComponent<Rigidbody2D>();
+
+		myRigidbody2D.AddForce(new Vector2(-speed,0),ForceMode2D.Impulse);
+		
+		
+		//can use this as well since for single frame only.
+//		myRigidbody2D.velocity = new Vector2(-speed,0);
+
+
+	}
+	
+	
+	
+	
+
+	private Rigidbody2D myRigidbody2D;
+
+}
